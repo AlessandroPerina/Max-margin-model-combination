413a9d03a2e6198e0bbe073b1ce5d59f5da789ff
Alessandro Perina <alessandro.perina@gmail.com>
2014-08-05 20:14:57 +0200

    v0.2 - Leggo .data

--- data_reader.py
+++ data_reader.py
@@ -19,10 +19,15 @@ class data_reader:
 		if (len(self.data[-1]) == 0):
 			del self.data[-1]
 
+		self.T = len( self.data )
 		self.Z = len( self.data[0] )
 		self.no_Z = [0 for i in range(0,self.Z )]
 		self.values_Z = [0 for i in range(0,self.Z )]
 		for z in range(0,self.Z ):
 			self.values_Z[z] = np.unique( [row[z] for row in self.data]) 
 			self.no_Z[z] = len(self.values_Z[z] )
+
+		self.X = np.zeros([Z,T])
+		
+
 
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ D = data_reader("nursery")
 print D.name
 print D.data[0]
 print D.no_Z
+print D.values_Z[1]
 print "Ho finito"
 
