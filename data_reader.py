c49ec20a2e39e10d1ee4f8416fc67c5798cb2be2
Alessandro Perina <alessandro.perina@gmail.com>
2014-08-05 18:49:18 +0200

    v0.2 - Leggo .data

--- data_reader.py
+++ data_reader.py
@@ -1,7 +1,28 @@
+from csv import reader
+import os
+import numpy as np
+
 class data_reader:
 
-	basepath = "./Datasets"
 	def __init__(self, name):
-		self.name = name
 
-		self.data
+		base_directory = os.path.abspath(".")
+		self.name = name
+		self.data = list()
+		os.chdir( ".\Datasets\\" + name )
+		with open( name + ".data", "rb" ) as f:
+			tmp = reader(f)
+			for idx,val in enumerate(tmp):
+				self.data.append(val)
+
+		os.chdir( base_directory )
+		if (len(self.data[-1]) == 0):
+			del self.data[-1]
+
+		self.Z = len( self.data[0] )
+		self.no_Z = [0 for i in range(0,self.Z )]
+		self.values_Z = [0 for i in range(0,self.Z )]
+		for z in range(0,self.Z ):
+			self.values_Z[z] = np.unique( [row[z] for row in self.data]) 
+			self.no_Z[z] = len(self.values_Z[z] )
+
---
+++ main.py
@@ -0,0 +1,11 @@
+#main
+import os
+from data_reader import *
+
+D = data_reader("nursery")
+
+print D.name
+print D.data[0]
+print D.no_Z
+print "Ho finito"
+
