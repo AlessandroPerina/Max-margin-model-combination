1c9f9f0defece4183fb2155d85727d25eaad671d
Alessandro Perina <alessandro.perina@gmail.com>
2014-12-01 16:47:36 +0100

    MLD - computation of cut-off point

--- data_reader.py
+++ data_reader.py
@@ -3,38 +3,96 @@ import os
 import numpy as np
 from collections import Counter
 
-def data_reader( name ):
-    base_directory = os.path.abspath(".")
-    data = list()
-    os.chdir( ".\Datasets\\" + name )
-    with open( name + ".data", "rb" ) as f:
-        tmp = reader(f)
-        for idx,val in enumerate(tmp):
-            data.append(val)
-    # Cancella le ultime linee vuote (se esistono)
-    while (len(data[-1]) == 0):
-        del data[-1]
+def data_reader( name, label_id = 'last', type = 'C', has_index = True ):
+    # C -> Categorical, N-> Numerical feature
+    # label_id -> index of the labels 'last' or 'first'
+    # has_index -> the first 'feature' is the ordinal index of the sample. Get rid of it
     
-    T = len( data )
-    Z = len( data[0] )
+    assert label_id == 'last' or label_id == 'first'
+    assert isinstance( has_index, bool)
+    assert  has_index and label_id == 'last'
 
-    names_Z = list()
-    features = np.zeros([T,Z])
-    for z in range( Z ):
-        listvalues = [row[z] for row in data]
-        if len( set( listvalues ) ) > 10:
-            names_Z.append('numeric')
-            features[:,z] = [float(w) for w in listvalues]
+    try:
+        base_directory = os.path.abspath(".")
+        data = list()
+        os.chdir( ".\Datasets\\" + name )
+        with open( name + ".data", "rb" ) as f:
+            tmp = reader(f)
+            for idx,val in enumerate(tmp):
+                data.append(val)
+        # Cancella le ultime linee vuote (se esistono)
+        while (len(data[-1]) == 0):
+            del data[-1]
+    
+        T = len( data )
+        Z = len( data[0] )
+        # Default. All the features are categorical
+        if type == 'N':
+            kind_Z = list(type)+['N']*(Z-len(type))
         else:
-            names_Z.append( list(set(listvalues)))
-            features[:,z] = [list(set(listvalues)).index( w ) for w in listvalues]
+            kind_Z = list(type)+['C']*(Z-len(type))
+   
 
-    os.chdir(base_directory)
-    return features, names_Z
+        names_Z = list()
+        features = np.zeros([T,Z])
+        for z in range( Z ):
+            listvalues = [row[z] for row in data]
+            if kind_Z[z] == 'N':
+                names_Z.append('numerical')
+                features[:,z] = [float(w) for w in listvalues]
+            else:
+                # If it is not numercial, it is categorical
+                names_Z.append( list(set(listvalues)))
+                features[:,z] = [list(set(listvalues)).index( w ) for w in listvalues]
+
+        if has_index:
+             y = features[:,-1]
+             C = len( set( y ))
+             X = features[:,1:-1]
+        else:
+            if label_id == 'last':
+                y = features[:,-1]
+                X = features[:,0:-1]
+            else:
+                y = features[:,0]
+                X = features[:,1:]
+
+        os.chdir(base_directory)
+        return features, names_Z, X, y
+    except:
+        print "Error somewhere!"
+        os.chdir('C:\Users\APerina\Desktop\Git\max-margin-model-combination')
 
 
-def numerical2category( listvalues, B ):
-    vals = [float( w ) for w in listvalues ]
-    
+def MDLdiscretize( x, y, no_values = 10):
+    pass
 
 
+def _MDLfind_partition( x,y ):
+    xsort = [a for (a,b) in sorted( zip(x,y) )]
+    ysort = [b for (a,b) in sorted( zip(x,y) )]
+    pass
+
+
+def _MLDpartition_value( xsort, ysort, idpartition ):
+    # Return the cut-off point
+
+    T = len(xsort)
+    countc = [ ysort.count(a) for a in set(ysort)]
+
+    boundary_point = list()
+    for p in range(1,T-1):
+        if ysort[p] != ysort[p-1]:
+            boundary_point.append(p)
+
+    E = np.zeros( len( boundary_point ));
+    for b in enumerate( boundary_point ):
+        tmp = ysort[0:b[1]+1]
+        pcs1 = [ float( tmp.count(a) ) for a in set(tmp)] / countc
+
+        tmp = ysort[b[1]+1:]
+        pcs2 = [ float( tmp.count(a) ) for a in set(tmp)] / countc
+
+        E[b[0]] = -( ((float(len(ysort[0:b[1]+1])-1))/T) *sum( pcs1*np.log( 1e-30 + pcs1 ) ) + ((float(len(ysort[b[1]+1:])-1))/T) *sum( pcs2*np.log( 1e-30 + pcs2 ) ) )
+
+    return xsort[b[E.argmin()]]
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #main
 import os
-import data_reader as DR
-
-name = 'nursery'
 os.chdir('C:\Users\APerina\Desktop\Git\max-margin-model-combination')
-features, names =  DR.data_reader( name )
+import data_reader as DR
+name = 'nursery'
+features, names =  DR.data_reader( name,  )
+data_reader( name, label_id = 'last', type = 'C', has_index = True )
 
 name = 'glass'
-features2, names2 =  DR.data_reader( name )
+features2, names2, X, y =  DR.data_reader( name, 'last', 'N', True )
 
 print "Ho finito"
 
